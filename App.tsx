391fd9f54bd47a071f5815a796126aa49eb26ecf
Masiu <ciszekmarcell@gmail.com>
2020-04-19 10:31:59 +0200

    react native starter

--- App.tsx
+++ App.tsx
@@ -1,22 +1,8 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+
+interface Props {}
+
+import {StyleSheet, View, Text, StatusBar, Image} from 'react-native';
 
 import {
   Header,
@@ -28,50 +14,33 @@ import {
 
 declare const global: {HermesInternal: null | {}};
 
-const App = () => {
+const App: React.FC<Props> = () => {
   return (
     <>
-      <StatusBar barStyle="dark-content" />
+      <View style={styles.container}>
+        <Text style={styles.textSize}>Hello</Text>
+        <Image
+          source={{uri: 'https://randomuser.me/api/portraits/men/1.jpg'}}
+          style={styles.img}
+        />
+      </View>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  textSize: {
+    fontSize: 40,
   },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  img: {
+    width: 100,
+    height: 100,
+    borderRadius: 100 / 2,
   },
 });
 
